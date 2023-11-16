54b1db0721d9767777e618f1873d454c31e3ce6b
PCFl-Tech <151001122+PCFl-Tech@users.noreply.github.com>
2023-11-16 01:17:01 -0500

    Update Trucks.js

--- Trucks.js
+++ Trucks.js
@@ -1,6 +1,6 @@
 function FindProxyForURL(url, host) {
 // If the hostname matches, send direct. 
-    if (shExpMatch(host, "*.googleusercontent.com|*.ggpht.com"))
+    if (shExpMatch(host, "*.googleusercontent.com|*.ggpht.com|*.googlevideo.com"))
        return "PROXY 127.0.0.1:8080"; 
 	   
  //default
