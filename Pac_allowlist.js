61c7bd2cf71bc2c78bf25819a5ea61c75ae3981a
PCFl-Tech <151001122+PCFl-Tech@users.noreply.github.com>
2023-11-17 00:34:00 -0500

    Create Pac_allowlist.js

---
+++ Pac_allowlist.js
@@ -0,0 +1,11 @@
+function FindProxyForURL(url, host) {
+// If the hostname matches, send direct. 
+    //if (shExpMatch(host, "*.googleusercontent.com|*.ggpht.com|*.googlevideo.com"))
+if (shExpMatch(host, "*.googleapis.com|*.gvt1.com|*.gvt2.com|*.manageengine.com|*.samsungknox.com|android.apis.google.com|apptics.zoho.com|clients3.google.com|connectivitycheck.gstatic.com|jproxy.zoho.com|play-lh.googleusercontent.com|play.google.com|self.events.data.microsoft.com|wa.me|www.google.com"))
+       //return "PROXY 127.0.0.1:8080";
+  return "DIRECT";
+	   
+ //default
+	//return "DIRECT";
+  return "PROXY 127.0.0.1:8080";
+}
