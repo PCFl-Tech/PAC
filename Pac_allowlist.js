a61e3769d81bc343e86ed27d98060810331d8a03
PCFl-Tech <151001122+PCFl-Tech@users.noreply.github.com>
2023-11-19 01:49:28 -0500

    Update Pac_allowlist.js

--- Pac_allowlist.js
+++ Pac_allowlist.js
@@ -1,6 +1,6 @@
 function FindProxyForURL(url, host) {
 // If the hostname matches, send direct. 
-    //if (shExpMatch(host, "*.googleusercontent.com|*.ggpht.com|*.googlevideo.com"))
+    //if (shExpMatch(host, "*.googleusercontent.com|*.ggpht.com|*.googlevideo.com|play.google.com|play-lh.googleusercontent.com"))
 if (shExpMatch(host, "*.manageengine.com|*.samsungknox.com|*.zoho.com"))
        //return "PROXY 127.0.0.1:8080";
   return "DIRECT";
